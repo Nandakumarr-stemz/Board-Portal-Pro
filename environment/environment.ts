62c0b2f7497c7f60bcc4df5166fabea5153c2430
Nandakumarr-stemz <nandakumarr@stemzglobal.com>
2026-02-18 12:44:44 +0530

    change basepath

--- environment/environment.ts
+++ environment/environment.ts
@@ -4,7 +4,7 @@ export const ACTIVE_ENV = "PROD"; // Change to "LOCAL", "UAT", or "PROD"
 export const environment = {
   production: true,
   apiUrl: "http://localhost:3000/api",
-  BASE_PATH: "/"
+  BASE_PATH: "/Board-Portal-Pro/"
 };
 
 // 
